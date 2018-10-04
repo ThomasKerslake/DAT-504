786b40a254b448199cf284632a602cb966ea878b
ThomasKerslake <33869524+ThomasKerslake@users.noreply.github.com>
2018-10-04 17:18:49 +0100

    .

--- DAT-504-ClassExercises/Week1/eloquent-javascript/chapter-02/Bean counting/js/sketch.js
+++ DAT-504-ClassExercises/Week1/eloquent-javascript/chapter-02/Bean counting/js/sketch.js
@@ -3,7 +3,7 @@ let userString = prompt('Enter a word');
 let userCharCount = prompt('What letter would you like me to count?');
 
 document.getElementById("myID").innerHTML = `The total number of, (${userCharCount}'s), in your word, (${userString}), is ${countChar(userString, userCharCount)} `;
-console.log(countChar(userString, userCharCount));
+console.log(countChar(userString, userCharCount)/2);
 
 function countChar(myString, char){
   for(let x = 0; x < myString.length; x++){
